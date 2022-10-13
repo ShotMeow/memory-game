0482c0b5f7b50c85ed8f7e5665db492bfeb10f79
ShotMeow <shotmeow@gmail.com>
2022-10-13 10:22:59 +0300

    Migrate Images to GitHub

--- src/components/App/App.data.ts
+++ src/components/App/App.data.ts
@@ -3,112 +3,112 @@ import { ICard } from "./App.interface";
 export const cardsData: ICard[] = [
     {
         id: 1,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/firebase1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/firebase1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 2
     },
     {
         id: 2,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/firebase2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/firebase2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 1
     },
     {
         id: 3,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/js1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/js1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 4
     },
     {
         id: 4,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/js2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/js2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 3
     },
     {
         id: 5,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/nginx1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/nginx1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 6
     },
     {
         id: 6,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/nginx2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/nginx2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 5
     },
     {
         id: 7,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/react1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/react1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 8
     },
     {
         id: 8,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/react2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/react2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 7
     },
     {
         id: 9,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/redux1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/redux1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 10
     },
     {
         id: 10,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/redux2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/redux2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 9
     },
     {
         id: 11,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/ts1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/ts1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 12
     },
     {
         id: 12,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/ts2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/ts2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 11
     },
     {
         id: 13,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/webpack1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/webpack1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 14
     },
     {
         id: 14,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/webpack2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/webpack2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 13
     },
     {
         id: 15,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/webstorm1.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/webstorm1.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 16
     },
     {
         id: 16,
-        imageUrl: 'https://git.lab.karpov.courses/d-mavrin/memory-game/-/raw/master/src/assets/images/webstorm2.png',
+        imageUrl: 'https://github.com/ShotMeow/memory-game/blob/master/src/assets/images/webstorm2.png?raw=true',
         isShow: false,
         guessed: false,
         relation: 15
